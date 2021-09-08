77ae90ffd09bc1e213667c4901cb019a35fdc6d0
dastrauss83 <dastrauss83@gmail.com>
2021-09-08 09:48:15 -0400

    roman

--- Easy.js
+++ Easy.js
@@ -100,4 +100,27 @@ const isPalindrome = (x) => {
 //-------------------------------------------------------------------------
 //Roman to Integer (13)
 
-const romanToInt = s;
+const romanToInt = (s) => {
+  let map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let answer = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < map[s[i + 1]]) answer -= map[s[i]];
+    else answer += map[s[i]];
+  }
+
+  return answer;
+};
+
+//-------------------------------------------------------------------------
+//Longest Common Prefix (14)
+
+const longestCommonPrefix = (strs) => {};
