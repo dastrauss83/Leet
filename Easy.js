5a88983b57671279f305131b6152508b6677d821
dastrauss83 <dastrauss83@gmail.com>
2021-09-08 09:53:47 -0400

    longest common prefix

--- Easy.js
+++ Easy.js
@@ -123,4 +123,16 @@ const romanToInt = (s) => {
 //-------------------------------------------------------------------------
 //Longest Common Prefix (14)
 
-const longestCommonPrefix = (strs) => {};
+const longestCommonPrefix = (strs) => {
+  if (strs.length === 1) return strs[0];
+
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+};
