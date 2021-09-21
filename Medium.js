959e11ef7745faef81fdaba6fc0b80572162614a
dastrauss83 <dastrauss83@gmail.com>
2021-09-21 08:17:28 -0400

    bfbs

--- Medium.js
+++ Medium.js
@@ -668,3 +668,42 @@ const invalidTransactions = function (transactions) {
   }
   return output;
 };
+
+//-------------------------------------------------------------------------
+//Letter Combinations of a Phone Number (17)
+
+const letterCombinations = (digits) => {
+  if (digits === null || digits.length === 0) {
+    return [];
+  }
+  const map = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  // We will use BFS to find all permutations
+  let queue = [""]; // queue contains all permutations in progress
+  let depth = 0;
+  while (queue.length > 0 && depth < digits.length) {
+    let levelLen = queue.length;
+    let letters = map[digits[depth]];
+    // Create all new permutations using existing permutations
+    // by appending possible character
+    for (let i = 0; i < levelLen; i++) {
+      let curStr = queue.shift();
+      for (let j = 0; j < letters.length; j++) {
+        queue.push(curStr + letters[j]);
+      }
+    }
+    depth++;
+  }
+  return queue;
+  // Time Complexity: O(3^m * 4^n),
+  // m = the number of digits that have 3 chars and n = the number of digits that have 4 chars
+  // Space Complexity: O(3^m * 4^n), queue can contain at most O(3^m * 4^n) elements
+};
