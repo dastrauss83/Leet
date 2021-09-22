91a149659564758c789d9e641d148ff2888e83d5
dastrauss83 <dastrauss83@gmail.com>
2021-09-22 19:36:01 -0400

    more

--- Easy.js
+++ Easy.js
@@ -963,3 +963,34 @@ const containsDuplicate = (nums) => {
 
   return false;
 };
+
+//-------------------------------------------------------------------------
+//Valid Palindrome II (680)
+
+const validPalindrome = (s) => {
+  let left = 0;
+  let right = s.length - 1;
+
+  const helper = (s, left, right) => {
+    while (left < right) {
+      if (s[left] !== s[right]) return false;
+      left++;
+      right--;
+    }
+    return true;
+  };
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return helper(s, left + 1, right) || helper(s, left, right - 1);
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+
+//-------------------------------------------------------------------------
+//Reorder Data in Log Files (937)
+
+const reorderLogFiles = (logs) => {};
