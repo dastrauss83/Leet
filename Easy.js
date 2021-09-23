6ed1ac6999dae5c4dfda1bf361fc92c9eb9fc9b4
dastrauss83 <dastrauss83@gmail.com>
2021-09-23 09:02:46 -0400

    mas

--- Easy.js
+++ Easy.js
@@ -993,4 +993,32 @@ const validPalindrome = (s) => {
 //-------------------------------------------------------------------------
 //Reorder Data in Log Files (937)
 
-const reorderLogFiles = (logs) => {};
+const reorderLogFiles = (logs) => {
+  const body = (s) => s.slice(s.indexOf(" ") + 1); // get the body after identifier
+  const isNum = (c) => /\d/.test(c);
+
+  // if body same then compare identifier
+  const compare = (a, b) => {
+    const n = body(a).localeCompare(body(b));
+    if (n !== 0) return n;
+    return a.localeCompare(b);
+  };
+
+  const digitLogs = [];
+  const letterLogs = [];
+  for (let log of logs) {
+    if (isNum(body(log))) digitLogs.push(log);
+    else letterLogs.push(log);
+  }
+  return [...letterLogs.sort(compare), ...digitLogs];
+};
+
+//-------------------------------------------------------------------------
+//Verifying an Alien Dictionary (953)
+
+const isAlienSorted = (words, sorted) => {
+  for (let i = 0; i < words.length - 1; i++) {
+    if (sorted.indexOf(words[i]) > sorted.indexOf(words[i + 1])) return false;
+  }
+  return true;
+};
