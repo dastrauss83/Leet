ba2829e110502996b444fe49701d55d03c028b71
dastrauss83 <dastrauss83@gmail.com>
2021-10-06 08:50:47 -0400

    easy

--- Easy.js
+++ Easy.js
@@ -1098,3 +1098,14 @@ const maxProfit = (prices) => {
 
 //-------------------------------------------------------------------------
 //FizzBuzz (412)
+const fizzBuzz = (n) => {
+  let ans = [];
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+    if (i % 3 === 0) str += "Fizz";
+    if (i % 5 === 0) str += "Buzz";
+    if (str) ans.push(str);
+    else ans.push(String(i));
+  }
+  return ans;
+};
